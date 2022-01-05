9327e73229989d77b8f9e6f953a41fac2610abc8
Kaan <92556652+kaankbt@users.noreply.github.com>
2022-01-06 01:22:53 +0300

    Bismillah
    project created

---
+++ fleetapp/src/test/java/com/kaankubat/fleetapp/FleetappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaankubat.fleetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
